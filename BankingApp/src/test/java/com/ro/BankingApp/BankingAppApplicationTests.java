6ce36489c6735078d233271da969093f3725632c
Ganesh Bellale <ganeshbellale45@gmail.com>
2024-10-17 22:51:27 +0530

    First Commit

---
+++ BankingApp/src/test/java/com/ro/BankingApp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ro.BankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
